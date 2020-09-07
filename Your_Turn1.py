2a797528fbffb3806b85e31efc8afb80ff8c0cb9
frankiesamsock <61481288+frankiesamsock@users.noreply.github.com>
2020-09-07 12:12:21 -0400

    Add files via upload

---
+++ Your_Turn1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+Location = "Documents/FLcensus9620.csv"
+df = pd.read_csv(Location, header=None)
+
+
+# In[7]:
+
+
+df.head()
+
+
+# In[8]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
